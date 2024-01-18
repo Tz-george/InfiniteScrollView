82252e746cb12a4810b8cb1c32cbe1db133ebfc3
george <1014175823@qq.com>
2024-01-18 13:45:43 +0800

    Initial Commit

---
+++ InfiniteScrollView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InfiniteScrollView
+//
+//  Created by 王庭志 on 2024/1/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InfiniteScrollView/InfiniteScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteScrollViewApp.swift
+//  InfiniteScrollView
+//
+//  Created by 王庭志 on 2024/1/18.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
